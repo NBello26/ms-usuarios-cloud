241b66d4bbe7eeb752da10399d3d2eb8ca06a3c2
nbello26 <nicolas.vicente1226@gmail.com>
2026-09-04 20:42:58 -0400

    feature - 1.1.0: Arreglo permisos cors v2

--- src/main/java/com/backend/usuarios/security/SecurityConfig.java
+++ src/main/java/com/backend/usuarios/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         // Permite orígenes dinámicos (localhost, API Gateway, etc.)
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("https://n2mnxks3b7.execute-api.us-east-1.amazonaws.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
